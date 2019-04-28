fe977b51d2af35d43fdc6f253499699709a07587
mondon11 <houjiateng@aliyun.com>
2019-04-28 21:46:13 +0800

    第2次提交，完善了chatflow

--- scenario.py
+++ scenario.py
@@ -5,6 +5,7 @@
 # @Software: PyCharm+python3
 
 from lru import LRU
+import json
 
 class Scenario:
     def __init__(self,act,ner,sessionId,lru_dict):
@@ -26,6 +27,17 @@ class Scenario:
         :param dict2:
         :return:dict
         '''
+
+        #去除dict里value的len是0的项
+        dict1_cp = dict1.copy()
+        dict2_cp = dict2.copy()
+        for item in dict1_cp.keys():
+            if len(dict1[item]) == 0:
+                dict1.pop(item)
+        for item in dict2_cp.keys():
+            if len(dict2[item]) == 0:
+                dict2.pop(item)
+
         dict = dict1.copy()
         dict.update(dict2)
         return dict
@@ -34,30 +46,92 @@ class Scenario:
         '''
         eg. dict = {'person':['小王'],'money':[100.00],'timespan':['2019-04-20 00:00:00','2019-04-21 00:00:00' ]}
             list = ['person','money']
-            res = true
+            res = true,[]
 
             dict = {'person':['小王'],'money':[],'timespan':['2019-04-20 00:00:00','2019-04-21 00:00:00' ]}
             list = ['person','money']
-            res = false
+            res = false,['money']
 
         :param dict: 
         :param list: 
-        :return: res
+        :return:
         '''
+        res = []
+        flag = True
         for item in list:
             if item not in dict.keys():
-                return False
+                res.append(item)
+                flag=False
             else:
                 if len(dict[item]) > 0:
                     pass
                 else:
-                    return False
-        return True
+                    res.append(item)
+                    flag = False
+        return flag,res
 
     def chatflow(self):
         #1.意图命中场景
         if self.act in self.act2slot:
             print('1')
+            #1.1 lru有该sessionId对应的cache
+            if self.sessionId in self.lru_dict.keys():
+                print('1.1')
+                #1.1.1 新老act一致
+                if self.act == self.lru_dict[self.sessionId]['act']:
+                    print('1.1.1')
+                    slot_fusion = self.dict_fusion(self.lru_dict[self.sessionId]['slot'],self.ner)
+                    completed,missed = self.value_completed(slot_fusion,self.act2slot[self.act])
+                    #1.1.1.1 新老NER融合后必填slot可补全
+                    if completed:
+                        print('1.1.1.1')
+                        print(self.act,slot_fusion)
+                        del self.lru_dict[self.sessionId]
+                    #1.1.1.2 新老NER融合后必填slot不能补全
+                    else:
+                        print('1.1.1.2')
+                        del self.lru_dict[self.sessionId]
+                        self.lru_dict[self.sessionId] = {
+                            'act':self.act,
+                            'slot':slot_fusion
+                        }
+                        print('请补全如下信息：' + str(missed))
+                #1.1.2 新老act不一致
+                else:
+                    print('1.1.2')
+                    completed,missed = self.value_completed(self.ner,self.act2slot[self.act])
+                    #1.1.2.1 新NER能补全必填slot
+                    if completed:
+                        print('1.1.2.1')
+                        print(self.act,self.ner)
+                        del self.lru_dict[self.sessionId]
+                    #1.1.2.2 新NER不能补全必填slot
+                    else:
+                        print('1.1.2.2')
+                        del self.lru_dict[self.sessionId]
+                        self.lru_dict[self.sessionId] = {
+                            'act':self.act,
+                            'slot':self.ner
+                        }
+                        print(self.lru_dict)
+                        print('请补全如下信息：' + str(missed))
+            #1.2 lru没有该sessionId对应的cache
+            else:
+                print('1.2')
+                completed,missed = self.value_completed(self.ner,self.act2slot[self.act])
+                #1.2.1 NER能补全必填slot
+                if completed:
+                    print('1.2.1')
+                    print(self.act,self.ner)
+                #1.2.2 NER不能补全必填slot
+                else:
+                    print('1.2.2')
+                    self.lru_dict[self.sessionId] = {
+                        'act':self.act,
+                        'slot':self.ner
+                    }
+                    print('请补全如下信息：'+str(missed))
+
 
         #2.意图没命中场景
         else:
@@ -68,8 +142,9 @@ class Scenario:
                 act_old = self.lru_dict[self.sessionId]['act']
                 slot_old = self.lru_dict[self.sessionId]['slot']
                 slot_fusion = self.dict_fusion(slot_old,self.ner)
+                completed,missed = self.value_completed(slot_fusion,self.act2slot[act_old])
                 #2.1.1 NER能补全cache
-                if self.value_completed(slot_fusion,self.act2slot[act_old]):
+                if completed:
                     print('2.1.1')
                     del self.lru_dict[self.sessionId]
                     print(act_old,slot_fusion)
@@ -88,6 +163,7 @@ class Scenario:
 
 def main():
     l = LRU(5)
+    '''
     l['00000000'] = {
         'act':'account_transfer',
         'slot':{
@@ -96,9 +172,15 @@ def main():
             'money':[]
         }
     }
-    scenario = Scenario('account_transfer1',{'person':['小王'],'timespan':[],'money':[1.00]},'00000000',l)
-    scenario.chatflow()
-
+    '''
+    # eg. input: ["account_detail",{"person":["ss"],"timespan":[],"money":[]},"00000000"]
+    while(1):
+        query = input('请输入问题：')
+        ls = json.loads(query)
+        scenario = Scenario(ls[0],ls[1],ls[2],l)
+        #scenario = Scenario('account_detail',{'person':['ss'],'timespan':[],'money':[]},'00000000',l)
+        scenario.chatflow()
+        print(l)
 
 if __name__ == '__main__':
     main()
